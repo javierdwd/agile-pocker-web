98699c9f5e28b8d6c38379626547be22779cd1d6
Javier Van Breedan <jvanbreedan@litebox.ai>
2019-07-09 10:53:57 -0300

    Configurar Firebase/Firestore

---
+++ src/store/firebase/firebase.js
@@ -0,0 +1,16 @@
+import * as firebase from "firebase/app";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCSX4fnSHVMt40e_harc9lxeFygicGWtB8",
+  authDomain: "agile-pocker-web.firebaseapp.com",
+  databaseURL: "https://agile-pocker-web.firebaseio.com",
+  projectId: "agile-pocker-web",
+  storageBucket: "",
+  messagingSenderId: "784778041351",
+  appId: "1:784778041351:web:ace77c431bf209c2"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
